633b750b7677d5b493b32f49fe175c92be0a1ccb
Your Name <alexandre.dalabona@gmail.com>
2019-04-19 12:13:00 -0300

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
+export class HomePage {
+  items: Array<any> = [
+    {
+      "name": "Novo Pedido"
+    },
+    {
+      "name": "Catálogo de Produtos"
+    },
+    {
+      "name": "Lista de Pedidos"
+    },
+    {
+      "name": "Lista de Clientes"
+    },
+    {
+      "name": "Nota Fiscal"
+    },
+  ]
 }
